ab5a02af456393fda575d140d091998c9d025ae3
Valentin <valentingaudin@gmail.com>
2024-02-07 21:29:26 +0100

    fix(app): make class final

--- app/Http/Controllers/Grades/GradeIndexController.php
+++ app/Http/Controllers/Grades/GradeIndexController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 
-class GradeIndexController extends Controller
+final class GradeIndexController extends Controller
 {
     public function __invoke(Request $request): JsonResponse
     {
