d2f9cdeaf9b21d512de7b1707af65eb21f6afb34
Valentin <valentingaudin@gmail.com>
2024-02-13 10:00:25 +0100

    feat(app): create auth helper to retrieve user information and grade data

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::shouldBeStrict(! $this->app->isProduction());
+
+        Validator::extend('plan_in_array', function ($attribute, $value, $parameters, $validator) {
+            return in_array($value, $parameters);
+        }, 'The :attribute must be one of :values.');
+
+        Validator::extend('token_between_values', function ($attribute, $value, $parameters, $validator) {
+            return $value >= trim($parameters[0]) && $value < trim($parameters[1]);
+        });
     }
 }
---
+++ app/Services/AuthHelperService.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use App\ValueObjects\GradeValueObject;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+final class AuthHelperService
+{
+    /**
+     * @param array<string, array<string, string|array<string, array<int, string>>>> $configuration
+     */
+    public function __construct(public array $configuration)
+    {
+    }
+
+    public function grade(): string|GradeValueObject
+    {
+        return match (true) {
+            $this->isFreeGrade() => GradeValueObject::make(...$this->configuration['free']),
+            $this->isFirstGrade() => GradeValueObject::make(...$this->configuration['first']),
+            $this->isSecondGrade() => GradeValueObject::make(...$this->configuration['second']),
+            $this->isThirdGrade() => GradeValueObject::make(...$this->configuration['third']),
+            default => 'No grade currently available',
+        };
+    }
+
+    private function checkAccessCondition(array $grade): bool
+    {
+        $grade = GradeValueObject::make(...$grade);
+
+        $accessPlanCondition = $grade->getPlan();
+        $minTokenAccess = $grade->getMinToken();
+        $maxTokenAccess = $grade->getMaxToken();
+
+        $validator = Validator::make([
+            'user_plan' => $this->user()->plan?->slug,
+            'user_token' => $this->user()->token_balance,
+        ], [
+            'user_plan' => [
+                'required',
+                'plan_in_array:'.implode(',', $accessPlanCondition),
+            ],
+            'user_token' => [
+                'required',
+                'numeric',
+                $maxTokenAccess
+                    ? 'token_between_values:'.$minTokenAccess.','.$maxTokenAccess
+                    : 'min:'.$minTokenAccess,
+            ],
+        ], [
+            'token_between_values' => "The :attribute must be superior to $minTokenAccess and exactly less $maxTokenAccess.",
+        ]);
+
+        return ! $validator->fails();
+    }
+
+    public function user(): User
+    {
+        $user = Auth::guard('sanctum')->user();
+
+        abort_if(boolean: ! $user, code: 401);
+
+        abort_if(boolean: ! $user instanceof User, code: 401);
+
+        return $user;
+    }
+
+    private function isFreeGrade(): bool
+    {
+        return $this->checkAccessCondition($this->configuration['free']);
+    }
+
+    private function isFirstGrade(): bool
+    {
+        return $this->checkAccessCondition($this->configuration['first']);
+    }
+
+    private function isSecondGrade(): bool
+    {
+        return $this->checkAccessCondition($this->configuration['second']);
+    }
+
+    private function isThirdGrade(): bool
+    {
+        return $this->checkAccessCondition($this->configuration['third']);
+    }
+}
